0b66128244da11111547d4bf3476e44047c08a84
fateme tavakoli <t.fateme@gmail.com>
2018-12-14 19:52:04 +0000

    Update proc.c scheduler to MLFQ

--- proc.c
+++ proc.c
@@ -324,37 +324,31 @@ wait(void)
 void
 scheduler(void)
 {
-  struct proc *p;
-  struct cpu *c = mycpu();
-  c->proc = 0;
-  
-  for(;;){
-    // Enable interrupts on this processor.
-    sti();
-
-    // Loop over process table looking for process to run.
-    acquire(&ptable.lock);
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
-        continue;
-
-      // Switch to chosen process.  It is the process's job
-      // to release ptable.lock and then reacquire it
-      // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
-
-      swtch(&(c->scheduler), p->context);
-      switchkvm();
-
-      // Process is done running for now.
-      // It should have changed its p->state before coming back.
-      c->proc = 0;
-    }
-    release(&ptable.lock);
-
-  }
+ for(;;){
+        // Enable interrupts on this processor.
+        sti();
+    
+        // Loop over process table looking for process to run.
+        acquire(&ptable.lock);
+        int priority;
+        for(priority = 0; priority <= PRIORITY_MAX; priority++) {
+            while(ptable.priCount[priority] > -1) {
+                proc = ptable.que[priority][0];
+                int i;
+                for (i = 0; i < ptable.priCount[priority]; i++) {
+                    ptable.que[priority][i] = ptable.que[priority][i + 1];
+                }
+                ptable.priCount[priority]--;
+                switchuvm(proc);
+                proc->state = RUNNING;
+                swtch(&cpu->scheduler, proc->context);
+                switchkvm();
+                proc = 0;
+                priority = 0;
+            }
+        }
+        release(&ptable.lock);
+ }
 }
 
 // Enter scheduler.  Must hold only ptable.lock
