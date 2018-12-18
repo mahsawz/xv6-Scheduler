1ed43f205a8b4d3bc49a196ce805704a20d9f73f
mahsa <mahsawz@gmail.com>
2018-12-18 08:29:43 +0000

    Update proc.h (add time)

--- proc.h
+++ proc.h
@@ -68,6 +68,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int ctime1,etime,rtime,iotime;
 };
 
 struct {
